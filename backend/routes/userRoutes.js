b56827755fb4f47424bb47c1527230e443751683
Ariadna Salomone <aricsalomone@gmail.com>
2026-05-09 23:58:24 -0300

    Agrego rate limiting

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const rateLimit = require("express-rate-limit");
 const userController = require("../controllers/userController");
 
-// Creo un enrutador
+//Creo un enrutador
 const router = express.Router();
 
-// RUTAS
+const loginLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutos
+    max: 5,
+    message: "Demasiados intentos de login. Intenta más tarde."
+});
+
+//RUTAS
 //POST /users/register: Registra usuarios con contraseñas hasheadas.
 router.post('/register', userController.registerUser);
 
 //POST /users/login: Devuelve un token de autenticación al iniciar sesión correctamente.
-router.post('/login', userController.loginUser);
+router.post('/login', loginLimiter, userController.loginUser);
 
 module.exports = router;
